c8b2252435d183f35537eda52c9b70cb5c53f54a
xiangxw <xiangxw5689@126.com>
2023-01-26 17:24:24 +0800

    to english

--- snake.py
+++ snake.py
@@ -4,28 +4,28 @@ from collections import deque
 import pygame
 import pygame.locals as LOCALS
 
-UP = 1 # 向上
-DOWN = 2 # 向下
-LEFT = 3 # 向左
-RIGHT = 4 # 向右
-SNAKE_WIDTH = 20 # 蛇的宽度，食物的宽度、蛇的移动速度和蛇的宽度一样
-RATE = 6 # 画面每秒刷新次数
-DISPLAY_WIDTH = 640 # 画面宽度
-DISPLAY_HEIGHT = 480 # 画面高度
-BACKGROUND_COLOR = pygame.Color(150, 150, 150) # 背景颜色
-SNAKE_COLOR = pygame.Color(139, 105, 105) # 蛇的颜色
-FOOD_COLOR = pygame.Color(165, 42, 42) # 食物颜色
-SCORE_COLOR = pygame.Color(0, 255, 0) # 分数的颜色
-GAME_OVER_COLOR = pygame.Color(255, 0, 0) # 游戏结束的颜色
+UP = 1 # up
+DOWN = 2 # down
+LEFT = 3 # left
+RIGHT = 4 # right
+SNAKE_WIDTH = 20 # width of snake, width of food, speed of snake
+RATE = 6 # fps
+DISPLAY_WIDTH = 640 # display width
+DISPLAY_HEIGHT = 480 # display height
+BACKGROUND_COLOR = pygame.Color(150, 150, 150) # background color
+SNAKE_COLOR = pygame.Color(139, 105, 105) # color of snake
+FOOD_COLOR = pygame.Color(165, 42, 42) # color of food
+SCORE_COLOR = pygame.Color(0, 255, 0) # color of score text
+GAME_OVER_COLOR = pygame.Color(255, 0, 0) # color game over text
 
 class Game(object):
     def __init__(self):
         super(Game, self).__init__()
-        self.display = None # 显示
-        self.font = None # 字体
-        self.clock = pygame.time.Clock() # 控制画面刷新频率
+        self.display = None # display
+        self.font = None # font
+        self.clock = pygame.time.Clock() # for fps control
         pygame.init()
-        pygame.display.set_caption("贪吃蛇(使用WSAD移动)")
+        pygame.display.set_caption("snake(move with WSAD)")
         pygame.mixer.init()
         pygame.mixer.music.load("sound/bg.ogg")
         pygame.mixer.music.set_volume(0.7)
@@ -39,22 +39,22 @@ class Game(object):
         self.playImageRect.centery = self.display.get_rect().centery + 100
         self.resetGame()
     
-    # 启动游戏
+    # start game
     def startGame(self):
         while True:
             self.tick()
             self.clock.tick(RATE)
     
-    # 重新开始游戏
+    # restart game
     def resetGame(self):
-        self.snake = deque([[100, 100]]) # 蛇身位置列表
-        self.direction = RIGHT # 方向
-        self.food = [300, 300] # 食物位置
-        self.score = 0 # 分数
-        self.gameOver = False # 游戏是否结束
+        self.snake = deque([[100, 100]]) # all parts of the snake
+        self.direction = RIGHT # direction
+        self.food = [300, 300] # position of food
+        self.score = 0 # score
+        self.gameOver = False # game is over or not
         pygame.mixer.music.play(-1)
     
-    # 结束游戏
+    # end game
     def endGame(self):
         pygame.quit()
         sys.exit()
@@ -66,7 +66,7 @@ class Game(object):
             self.check()
         self.draw()
     
-    # 处理事件
+    # handle event
     def handleEvent(self):
         for event in pygame.event.get():
             if event.type == LOCALS.QUIT:
@@ -86,7 +86,7 @@ class Game(object):
                 if self.gameOver and self.playImageRect.collidepoint(event.pos):
                     self.resetGame()
     
-    # 移动
+    # move
     def move(self):
         head = [self.snake[0][0], self.snake[0][1]]
         if self.direction == UP:
@@ -97,7 +97,7 @@ class Game(object):
             head[0] -= SNAKE_WIDTH
         else:
             head[0] += SNAKE_WIDTH
-        if self.willCollide(self.snake[0], self.food): # 吃到食物
+        if self.willCollide(self.snake[0], self.food): # eat food
             self.score += 1
             self.eatSound.play()
             self.genFood()
@@ -105,61 +105,61 @@ class Game(object):
             self.snake.pop()
         self.snake.appendleft(head)
     
-    # 检查
+    # check
     def check(self):
-        # 检查游戏是否结束
+        # check game over
         if self.isGameEnd():
             self.gameOver = True
             self.dieSound.play()
             pygame.mixer.music.stop()
     
-    # 绘图
+    # draw
     def draw(self):
-        # 背景
+        # background
         self.display.fill(BACKGROUND_COLOR)
-        # 蛇
+        # snake 
         for part in self.snake:
             pygame.draw.rect(self.display, SNAKE_COLOR, LOCALS.Rect(part[0], part[1], SNAKE_WIDTH, SNAKE_WIDTH))
-        # 食物
+        # food
         pygame.draw.rect(self.display, FOOD_COLOR, LOCALS.Rect(self.food[0], self.food[1], SNAKE_WIDTH, SNAKE_WIDTH))
-        # 分数
-        self.drawText("分数: %d" % self.score, SCORE_COLOR, (10, 10))
-        # 游戏结束
+        # score
+        self.drawText("Score: %d" % self.score, SCORE_COLOR, (10, 10))
+        # game over
         if self.gameOver:
-            self.drawText("游戏结束!", GAME_OVER_COLOR, (DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2), 'center')
+            self.drawText("Game Over!", GAME_OVER_COLOR, (DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2), 'center')
             self.display.blit(self.playImage, self.playImageRect)
         
-        # 刷新
+        # refresh
         pygame.display.flip()
     
-    # 写字
+    # draw text
     def drawText(self, text, color, pos, posType = "topleft"):
         scoreSurf = self.font.render(text, True, color)
         scoreRect = scoreSurf.get_rect()
         setattr(scoreRect, posType, pos)
         self.display.blit(scoreSurf, scoreRect)
     
-    # 游戏是否结束
+    # is game over
     def isGameEnd(self):
-        # 碰到边界
+        # touching the boundary
         if self.snake[0][0] < 0 or self.snake[0][0] > DISPLAY_WIDTH - SNAKE_WIDTH \
             or self.snake[0][1] < 0 or self.snake[0][1] > DISPLAY_HEIGHT - SNAKE_WIDTH:
             return True
 
-        # 碰到自己
+        # touching the snake
         for i in range(1, len(self.snake)):
             if self.willCollide(self.snake[0], self.snake[i]):
                 return True
 
         return False
 
-    # 是否会发生碰撞
+    # will collide
     def willCollide(self, pos1, pos2):
         rect1 = LOCALS.Rect(pos1[0], pos1[1], SNAKE_WIDTH, SNAKE_WIDTH)
         rect2 = LOCALS.Rect(pos2[0], pos2[1], SNAKE_WIDTH, SNAKE_WIDTH)
         return rect1.colliderect(rect2)
     
-    # 产生食物
+    # gen food
     def genFood(self):
         while True:
             x = random.randint(0, DISPLAY_WIDTH - SNAKE_WIDTH)
